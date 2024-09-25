ff38d9fd1446837fb478338cf7fc7c04fe8395b1
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2024-09-25 22:06:37 +0100

    Remove suppressions

--- benchmarks/Benchmarks.cs
+++ benchmarks/Benchmarks.cs
@@ -6,6 +6,7 @@ BenchmarkRunner.Run<Benchmarks>(args: args);
 [IterationTime(1000)]
 [MemoryDiagnoser]
 [ShortRunJob]
+#pragma warning disable CA1050 // Declare types in namespaces
 public partial class Benchmarks
 {
 }
