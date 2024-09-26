2f79570df219cb75f26dcf580a39d0b607a9e368
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2024-09-26 21:04:31 +0100

    Comment back in benchmarks

--- benchmarks/Benchmarks.QrCodeBenchmarks.cs
+++ benchmarks/Benchmarks.QrCodeBenchmarks.cs
@@ -14,22 +14,22 @@ public partial class Benchmarks
     [Benchmark(Description = "Quarer")]
     public QrCode Quarer() => QrCode.Create(Data, ErrorCorrectionLevel.M);
 
-    //[Benchmark(Description = "Apose.BarCode")]
-    //public void AposeCode()
-    //{
-    //    using var generator = new Aspose.BarCode.Generation.BarcodeGenerator(Aspose.BarCode.Generation.EncodeTypes.QR, Data);
-    //    generator.Parameters.Barcode.QR.QrErrorLevel = Aspose.BarCode.Generation.QRErrorLevel.LevelM;
-    //    generator.Save(Stream.Null, Aspose.BarCode.Generation.BarCodeImageFormat.Png);
-    //}
+    [Benchmark(Description = "Apose.BarCode")]
+    public void AposeCode()
+    {
+        using var generator = new Aspose.BarCode.Generation.BarcodeGenerator(Aspose.BarCode.Generation.EncodeTypes.QR, Data);
+        generator.Parameters.Barcode.QR.QrErrorLevel = Aspose.BarCode.Generation.QRErrorLevel.LevelM;
+        generator.Save(Stream.Null, Aspose.BarCode.Generation.BarCodeImageFormat.Png);
+    }
 
-    //[Benchmark(Description = "QrCode.Net")]
-    //public Gma.QrCodeNet.Encoding.QrCode GmaQrCodeNet() => new Gma.QrCodeNet.Encoding.QrEncoder().Encode(Data);
+    [Benchmark(Description = "QrCode.Net")]
+    public Gma.QrCodeNet.Encoding.QrCode GmaQrCodeNet() => new Gma.QrCodeNet.Encoding.QrEncoder().Encode(Data);
 
-    //[Benchmark(Description = "QrCoder")]
-    //public QRCoder.QRCodeData QrCoder() => QRCoder.QRCodeGenerator.GenerateQrCode(Data, QRCoder.QRCodeGenerator.ECCLevel.Default);
+    [Benchmark(Description = "QrCoder")]
+    public QRCoder.QRCodeData QrCoder() => QRCoder.QRCodeGenerator.GenerateQrCode(Data, QRCoder.QRCodeGenerator.ECCLevel.Default);
 
-    //[Benchmark(Description = "SkiaSharp.QrCode")]
-    //public SkiaSharp.QrCode.QRCodeData SkiaSharpQrCode() => SkiaGenerator.CreateQrCode(Data, SkiaSharp.QrCode.ECCLevel.M);
+    [Benchmark(Description = "SkiaSharp.QrCode")]
+    public SkiaSharp.QrCode.QRCodeData SkiaSharpQrCode() => SkiaGenerator.CreateQrCode(Data, SkiaSharp.QrCode.ECCLevel.M);
 
     [Benchmark(Description = "ZXing.Net", Baseline = true)]
     public ZXing.QrCode.Internal.QRCode ZXingQrCode() => ZXing.QrCode.Internal.Encoder.encode(Data, ZXing.QrCode.Internal.ErrorCorrectionLevel.M);
