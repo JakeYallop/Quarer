420a4e79101aa4e6ea5e79c797cff50b9056d680
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2024-10-15 21:05:33 +0100

    Move to BenchmarkSwitcher

--- benchmarks/Benchmarks.cs
+++ benchmarks/Benchmarks.cs
@@ -1,12 +1,11 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<Benchmarks>(args: args);
+BenchmarkSwitcher.FromAssembly(typeof(Benchmarks).Assembly).Run(args: args);
 
 [HideColumns("RatioSD", "StdDev")]
 [MemoryDiagnoser]
 [ShortRunJob]
-[HtmlExporter]
 [Outliers(Perfolizer.Mathematics.OutlierDetection.OutlierMode.RemoveAll)]
 #pragma warning disable CA1050 // Declare types in namespaces
 public partial class Benchmarks
